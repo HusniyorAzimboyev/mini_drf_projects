b77ee16328d5c19b86b05bb33d5261a3c4926a66
husniyorazimboyev <husniyor09@gmail.com>
2025-02-21 15:41:10 +0500

    modified

--- todo_list/config/settings.py
+++ todo_list/config/settings.py
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'hchchcjcjvhvjcjcjvjvjv@gmail.com'
-EMAIL_HOST_PASSWORD = "qkxeyqxcazwnpwun"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587  #465
 EMAIL_USE_TLS = True
--- todo_list/tasks/models.py
+++ todo_list/tasks/models.py
@@ -4,8 +4,14 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Task(models.Model):
+    statusChoices = (
+        (1,1),
+        (0,0)
+    )
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True,null=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE,blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.SmallIntegerField(choices=statusChoices) # 0 means not done, 1 means done.
     def __str__(self):
-        return f"{self.user.name}'s - {self.title}"
+        return f"{self.user.username}'s - {self.title}"
