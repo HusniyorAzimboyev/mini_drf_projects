bf68909f279aee9c049140d2a94f9502005fc19b
husniyorazimboyev <husniyor09@gmail.com>
2025-03-03 17:35:22 +0500

    modified

--- weather_api_wrapper/api/models.py
+++ weather_api_wrapper/api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Requests(models.Model):
+class Request(models.Model):
     request = models.CharField(max_length=1000)
     response = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
--- weather_api_wrapper/api/views.py
+++ weather_api_wrapper/api/views.py
@@ -1,11 +1,15 @@
 from rest_framework import views
 from rest_framework.response import Response
 from .services import get_weather
+from .models import Request
 
 class WeatherInfo(views.APIView):
     def post(self, request):
         city = request.data.get("city")
         current = get_weather(city=city)
+
+        Request.objects.create(request=request.url)
+
         return Response({"current_temp":f'{current}'})
     def get(self,request):
         return Response({"message":"Please send POST request and provide your city in it"})
